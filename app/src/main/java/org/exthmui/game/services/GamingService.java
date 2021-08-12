db3f5a4542d4a05719cfc0dc43c2333dc07de67f
David Setiawan <valkry24@gmail.com>
2021-08-12 14:48:05 +0000

    GamingMode: Nuke HW keys support
    Signed-off-by: David Setiawan <valkry24@gmail.com>

--- app/src/main/java/org/exthmui/game/services/GamingService.java
+++ app/src/main/java/org/exthmui/game/services/GamingService.java
@@ -116,8 +116,6 @@ public class GamingService extends Service {
                 setDisableAutoBrightness(intent.getBooleanExtra("value", Constants.ConfigDefaultValues.DISABLE_AUTO_BRIGHTNESS), false);
             } else if (Constants.GamingActionTargets.DISABLE_GESTURE.equals(target)) {
                 setDisableGesture(intent.getBooleanExtra("value", Constants.ConfigDefaultValues.DISABLE_GESTURE));
-            } else if (Constants.GamingActionTargets.DISABLE_HW_KEYS.equals(target)) {
-                setDisableHwKeys(intent.getBooleanExtra("value", Constants.ConfigDefaultValues.DISABLE_HW_KEYS), false);
             } else if (Constants.GamingActionTargets.DISABLE_RINGTONE.equals(target)) {
                 setDisableRingtone(intent.getBooleanExtra("value", Constants.ConfigDefaultValues.DISABLE_RINGTONE));
             } else if (Constants.GamingActionTargets.SHOW_DANMAKU.equals(target)) {
@@ -232,10 +230,8 @@ public class GamingService extends Service {
             mCurrentConfig.putInt(Constants.ConfigKeys.PERFORMANCE_LEVEL, performanceLevel);
         }
 
-        // hw keys & gesture
-        boolean disableHwKeys = getBooleanSetting(Constants.ConfigKeys.DISABLE_HW_KEYS, Constants.ConfigDefaultValues.DISABLE_HW_KEYS);
+        // gesture
         boolean disableGesture = getBooleanSetting(Constants.ConfigKeys.DISABLE_GESTURE, Constants.ConfigDefaultValues.DISABLE_GESTURE);
-        setDisableHwKeys(disableHwKeys, false);
         setDisableGesture(disableGesture);
 
         // quick-start apps
@@ -257,22 +253,6 @@ public class GamingService extends Service {
         Intent intent = new Intent(Constants.Broadcasts.BROADCAST_CONFIG_CHANGED);
         intent.putExtras(mCurrentConfig);
         LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
-    }
-
-    private void setDisableHwKeys(boolean disable, boolean restore) {
-        if (mLineageHardware == null) return;
-        if (!mCurrentConfig.containsKey("old_disable_hw_keys")) {
-            boolean oldValue = mLineageHardware.get(LineageHardwareManager.FEATURE_KEY_DISABLE);
-            mCurrentConfig.putBoolean("old_disable_hw_keys", oldValue);
-        }
-        if (!restore) {
-            mCurrentConfig.putBoolean(Constants.ConfigKeys.DISABLE_HW_KEYS, disable);
-            mLineageHardware.set(LineageHardwareManager.FEATURE_KEY_DISABLE, disable);
-        } else {
-            boolean oldValue = mCurrentConfig.getBoolean("old_disable_hw_keys");
-            mCurrentConfig.putBoolean(Constants.ConfigKeys.DISABLE_HW_KEYS, oldValue);
-            mLineageHardware.set(LineageHardwareManager.FEATURE_KEY_DISABLE, oldValue);
-        }
     }
 
     private void setDisableGesture(boolean disable) {
@@ -340,7 +320,6 @@ public class GamingService extends Service {
         if (mMenuOverlay) stopServiceAsUser(mOverlayServiceIntent, UserHandle.CURRENT);
         mTelephonyManager.listen(mPhoneStateListener, PhoneStateListener.LISTEN_NONE);
         setDisableGesture(false);
-        setDisableHwKeys(false, true);
         setDisableAutoBrightness(false, true);
         setDisableRingtone(false);
         setPerformanceLevel(-1);
